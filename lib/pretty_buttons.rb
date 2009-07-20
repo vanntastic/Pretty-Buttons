ee8a3640eda6098dcfab8885cd582babc906eb84
vann <vann@innerfusion.net>
2009-07-20 16:59:48 -0500

    added the submit-line class to the submit container and allowed the ability to pass blocks to pretty_submit

--- lib/pretty_buttons.rb
+++ lib/pretty_buttons.rb
@@ -1,9 +1,9 @@
 module PrettyButtons
   
+  # gives you the stylesheet link for the buttons
   def include_pretty_buttons
     stylesheet_link_tag 'buttons'
   end
-  
   
   # generates a pretty button
   # USAGE : 
@@ -13,14 +13,39 @@ module PrettyButtons
     link_to "<span>#{content}</span>", path, options
   end
   
-  # generates a pretty submit button
+  # generates a pretty submit button inside a container
   # USAGE :
-  #   pretty_submit "Sign Up!"
-  def pretty_submit(content, options={})
-    options[:id] ||= "#{rand(9999)}-submit"
-    options[:onclick] ||= "document.getElementById(this.id).parentNode.submit();"
-    options[:class] = options[:class].nil? ? "button" : "button " << options[:class]
-    link_to "<span>#{content}</span>", '#', options
+  #   pretty_submit "Sign Up!" # => generates a pretty submit button for the parent form
+  # 
+  # if you need to generate a submit button for a more complex form, here's an example:
+  #   pretty_submit "Sign Up!", :form => "signup-form"
+  # 
+  # NOTE : 
+  #   this method uses javascript to submit the form, so this requires that you only
+  #   nest the the submit button one element inside the the form, if you need this to 
+  #   work for a more complex form where the submit button is nested more than one level
+  #   deep then simply pass the id of the form to the :form option
+  
+  def pretty_submit(content, options={}, &blk)
+    default_form = "document.getElementById(this.id).parentNode.parentNode"
+    options[:form] ||= default_form
+    form = options[:form] == default_form ? default_form : "document.getElementById('#{options[:form]}')"
+    
+    options[:btn] ||= {}
+    options[:btn][:id] ||= "submit-#{rand(9999)}"
+    options[:btn][:class] = options[:btn][:class].nil? ? "button" : ("button " << options[:btn][:class])
+    options[:btn][:onclick] = "#{form}.submit();"
+    
+    options[:div] ||= {}
+    options[:div][:class] = options[:div][:class].nil? ? "clear submit-line" : ("clear submit-line " << options[:div][:class])
+    
+    options.delete :form
+    btn = link_to("<span>#{content}</span>", '#', options[:btn])
+    if block_given?
+      concat content_tag(:div, "#{btn}#{capture(&blk)}", options[:div]), blk.binding
+	  else
+	    content_tag :div, btn, options[:div]
+    end
   end
   
 end
