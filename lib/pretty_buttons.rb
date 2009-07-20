76f15c660b8164c60b8deff2ab2bc3b01133af6a
vann <vann@innerfusion.net>
2009-07-20 15:51:33 -0500

    added in the pretty_submit method for submit buttons

--- lib/pretty_buttons.rb
+++ lib/pretty_buttons.rb
@@ -5,9 +5,22 @@ module PrettyButtons
   end
   
   
+  # generates a pretty button
+  # USAGE : 
+  #   pretty_button 'Click me', signup_url
   def pretty_button(content, path, options={})
     options[:class] = options[:class].nil? ? "button" : "button " << options[:class]
     link_to "<span>#{content}</span>", path, options
+  end
+  
+  # generates a pretty submit button
+  # USAGE :
+  #   pretty_submit "Sign Up!"
+  def pretty_submit(content, options={})
+    options[:id] ||= "#{rand(9999)}-submit"
+    options[:onclick] ||= "document.getElementById(this.id).parentNode.submit();"
+    options[:class] = options[:class].nil? ? "button" : "button " << options[:class]
+    link_to "<span>#{content}</span>", '#', options
   end
   
 end
